51b99416a253ed63a166d163653682da8c1cc2a3
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-02-02 09:12:28 +0900

    feat: Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import List from 'containers/pages/List';
 import TastingSheet from 'containers/pages/TastingSheet';
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <TastingSheet wineType="red" />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/tasting-sheet">
+          <TastingSheet wineType="red" />
+        </Route>
+        <Route path="/">
+          <List />
+        </Route>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/containers/pages/List.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const List: FC = () => <p>List page</p>;
+
+export default List;
