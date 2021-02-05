307836bb0d6e98a17a01280986119ea3f088475e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-02-05 09:48:17 +0900

    feat: Add Title to the list

--- src/components/pages/List.tsx
+++ src/components/pages/List.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Header } from 'semantic-ui-react';
 import ListItem from 'components/organisms/ListItem';
 
 const List: FC = () => (
-  <Card.Group>
-    <ListItem />
-  </Card.Group>
+  <>
+    <Header as="h1" textAlign="center">
+      テイスティングリスト
+    </Header>
+    <Card.Group>
+      <ListItem />
+    </Card.Group>
+  </>
 );
 
 export default List;
