974ee2a577473aa301cb7e3bd88672c8623df173
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-02-05 09:48:47 +0900

    feat: Add WineTypeDot component

---
+++ src/components/molecules/WineTypeDot.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const WineTypeDot: FC = () => <Icon name="circle" color="purple" />;
+
+export default WineTypeDot;
--- src/components/organisms/ListItem.tsx
+++ src/components/organisms/ListItem.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
 import { Card } from 'semantic-ui-react';
+import WineTypeDot from 'components/molecules/WineTypeDot';
+
+const style = {
+  flexDirection: 'row',
+  alignItems: 'center',
+  height: '3rem',
+};
 
 const ListItem: FC = () => (
   <>
-    <Card fluid>
+    <Card fluid style={style}>
+      <WineTypeDot />
       <p>2012/02/02 9:35</p>
     </Card>
     <Card fluid>
