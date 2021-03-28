1f2342ea119c91434f3a262079c3231ca066bebc
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-03-28 14:59:43 +0900

    refactor: Organize Route on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { FC, useContext } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { Container } from 'semantic-ui-react';
 
@@ -8,25 +8,27 @@ import TastingList from 'containers/pages/TastingList';
 import TastingSheet from 'containers/pages/TastingSheet';
 
 import paths from 'paths';
+import { UserContext } from './contexts';
 
 const App: FC = () => {
+  const { credential } = useContext(UserContext);
+
   return (
     <div className="App">
       <Container style={{ padding: '1rem 0' }}>
-        <Switch>
-          <Route path={paths.tasting}>
-            <TastingSheet />
-          </Route>
-          <Route path={paths.tastingNew}>
-            <TastingSheet />
-          </Route>
-          <Route path={paths.signIn}>
-            <SignIn />
-          </Route>
-          <Route path={paths.list}>
-            <TastingList />
-          </Route>
-        </Switch>
+        {!credential ? (
+          <Switch>
+            <Route path={paths.signIn} component={SignIn} />
+            <Redirect to={paths.signIn} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path={paths.tasting} component={TastingSheet} />
+            <Route path={paths.tastingNew} component={TastingSheet} />
+            <Route path={paths.list} component={TastingList} />
+            <Redirect to={paths.list} />
+          </Switch>
+        )}
       </Container>
     </div>
   );
