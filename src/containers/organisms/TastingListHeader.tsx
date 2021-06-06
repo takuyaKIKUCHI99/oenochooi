47bd87d65df098f617fd59efc81326d8e85d4596
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-06-06 15:51:25 +0900

    feat: Add Avator dropdown menu on the app header

--- src/containers/organisms/TastingListHeader.tsx
+++ src/containers/organisms/TastingListHeader.tsx
@@ -1,8 +1,8 @@
 import React, { FC, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { FirebaseContext } from 'contexts';
+import { FirebaseContext, UserContext } from 'contexts';
 
-import { Button, Dropdown } from 'semantic-ui-react';
+import { Dropdown, Image } from 'semantic-ui-react';
 import PageHeader from 'components/organisms/PageHeader';
 
 import { WineType } from 'data/tastingSheet';
@@ -11,6 +11,7 @@ import paths from 'paths';
 
 const EnhancedTastingListHeader: FC = () => {
   const { auth } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
   const history = useHistory();
 
   const handleClick = (wineType: WineType) => {
@@ -20,36 +21,43 @@ const EnhancedTastingListHeader: FC = () => {
     });
   };
 
-  const actionElement = (
-    <>
-      <Dropdown text="テイスティングシート作成">
-        <Dropdown.Menu>
-          <Dropdown.Item
-            text="赤ワイン"
-            label={{ color: 'purple', empty: true, circular: true }}
-            onClick={() => handleClick('red')}
-          />
-          <Dropdown.Item
-            text="白ワイン"
-            label={{ color: 'yellow', empty: true, circular: true }}
-            onClick={() => handleClick('white')}
-          />
-        </Dropdown.Menu>
-      </Dropdown>
-      <Button
-        basic
-        style={{ marginLeft: '1rem' }}
-        onClick={() => auth?.signOut()}
-      >
-        Logout
-      </Button>
-    </>
-  );
+  const actionElement = () => {
+    const trigger = (
+      <span>
+        <Image avatar src={user?.photoURL} /> {user?.displayName}
+      </span>
+    );
+
+    return (
+      <>
+        <Dropdown text="テイスティングシート作成">
+          <Dropdown.Menu>
+            <Dropdown.Item
+              text="赤ワイン"
+              label={{ color: 'purple', empty: true, circular: true }}
+              onClick={() => handleClick('red')}
+            />
+            <Dropdown.Item
+              text="白ワイン"
+              label={{ color: 'yellow', empty: true, circular: true }}
+              onClick={() => handleClick('white')}
+            />
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown trigger={trigger}>
+          <Dropdown.Menu>
+            <Dropdown.Item text="Logout" onClick={() => auth?.signOut()} />
+          </Dropdown.Menu>
+        </Dropdown>
+      </>
+    );
+  };
 
   return (
     <PageHeader
       headerTitle="テイスティングリスト"
-      actionElement={actionElement}
+      actionElement={actionElement()}
     />
   );
 };
